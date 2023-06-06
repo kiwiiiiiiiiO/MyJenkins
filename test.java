05777d1b74d521f67de899d9c580056df4542565
kiwiiiiiiiiO <94119970+kiwiiiiiiiiO@users.noreply.github.com>
2023-06-06 21:53:36 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
-public class HelloWorld
+public class Message
 {
    public static void main(String[] args)
    {
-      System.out.println("My Message");  // Noncompliant
+      logger.log("My Message");
    }
 }
